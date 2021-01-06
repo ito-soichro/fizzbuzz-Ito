0aa86034d32da2ea7d530828032ce1374471f69c
ito-soichro <queen.fm1235@gmail.com>
2021-01-06 15:54:09 +0900

    fix_index.test.ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,16 @@
 import { printOneToHundred } from '../index';
 
-let num: number;
-let count: number = printOneToHundred(); 
 
 describe('1から100を出力', () => {
   test('count 1 to 100', () => {
-    for( num = 1; num <= 100; num++){
-      if(num === 101){
-        expect(count).toBe(num);
-      }
-    }
+    const spyLog = jest.spyOn(console, 'log')
+    const array = spyLog.mock.calls
+    printOneToHundred()
+    expect(array).toHaveLength(100)
+    expect(array[0][0]).toBe(1)
+    expect(array[99][0]).toBe(100)
+
+    spyLog.mockReset()
+    spyLog.mockRestore()
   });
 });
