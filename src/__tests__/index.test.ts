8bb7039007ab449b56f4756177c798d0dcb53522
ito-soichro <queen.fm1235@gmail.com>
2021-01-06 12:04:13 +0900

    count_1_100

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,14 @@
-import { Counter } from '../index';
+import { printOneToHundred } from '../index';
 
-test('count 1 to 100', () => {
-  const arr1_100 = Array.from(new Array(100)).map((v,i)=> i + 1);
-  const counter = new Counter()
-  expect(JSON.stringify(counter.oneToHundred())).toBe(JSON.stringify(arr1_100));
+let num: number;
+let count: number = printOneToHundred(); 
+
+describe('1から100を出力', () => {
+  test('count 1 to 100', () => {
+    for( num = 1; num <= 100; num++){
+      if(num === 101){
+        expect(count).toBe(num);
+      }
+    }
+  });
 });
