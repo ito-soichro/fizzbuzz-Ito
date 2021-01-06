cc8db915a95bd6fabf1558e3e8b36782476d59b2
ito-soichro <queen.fm1235@gmail.com>
2021-01-06 17:53:46 +0900

    fix_index.test.ts 3

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,8 +2,8 @@ import { printOneToHundred } from '../index';
 
 const spyLog = jest.spyOn(console, 'log')
 const array = spyLog.mock.calls
-printOneToHundred()
 
+printOneToHundred()
 
 describe('1から100を出力', () => {
   test('count 1 to 100', () => {
@@ -11,6 +11,10 @@ describe('1から100を出力', () => {
     expect(array).toHaveLength(100)
     expect(array[0][0]).toBe(1)
     expect(array[array.length-1][0]).toBe(100)
+    expect(typeof array).toEqual('object')
+
+    let array2 = new Set(array);
+    expect(array2.size).toEqual(array.length)
 
     spyLog.mockReset()
     spyLog.mockRestore()
