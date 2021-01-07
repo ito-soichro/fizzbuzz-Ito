004c714f315fd73afbc4ec45ae8530be1dcd20e6
ito-soichro <queen.fm1235@gmail.com>
2021-01-07 14:25:11 +0900

    fix_index.test.ts 5

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,31 +1,22 @@
 import { printOneToHundred } from '../index';
 
 const spyLog = jest.spyOn(console, 'log')
-// const array = spyLog.mock.calls
+const array = spyLog.mock.calls
+const array3 = spyLog.mock.calls.map((arr) => arr[0])
 
 printOneToHundred()
 
- // 全部を配列に詰め直してみます
- const actual = spyLog.mock.calls.map((arr) => arr[0])
-
 describe('1から100を出力', () => {
   test('count 1 to 100', () => {
-    // 要素数は100
-    expect(actual).toHaveLength(100)
-     // 一意な要素数も100
-    expect(Math.max(...actual)).toBe(100)
-    // 最小は0
-    expect(Math.min(...actual)).toBe(1)
-    // 最大は100
-    expect(new Set(actual).size).toBe(100)
-    // 全部整数
-    expect(actual.every((arg) => Number.isInteger(arg))).toBeTruthy()
-    // 昇順ソートされている
-    expect(actual).toEqual(actual.slice().sort((a, b) => a - b))
-
-
+    
+    expect(array).toHaveLength(100)
+    expect(array[0][0]).toBe(1)
+    expect(array[array.length-1][0]).toBe(100)
+    expect(typeof array).toEqual('object')
+    expect(array3.every((arg) => Number.isInteger(arg))).toBeTruthy()
+    expect(array3).toEqual(array3.slice().sort((a, b) => a - b))
 
     spyLog.mockReset()
     spyLog.mockRestore()
   });
-})
+});
