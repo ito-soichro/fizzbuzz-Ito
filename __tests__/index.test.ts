c63499815565d112b0043f33a153fcfb50a133a6
ito-soichro <queen.fm1235@gmail.com>
2021-01-15 17:44:11 +0900

    fix_#1-3_refactoring_FizzBuzz 3

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -3,28 +3,14 @@ import  printOneToHundred  from'../src/printOneToHundred';
 
 describe('Depending on the condition, "Fizz", "Buzz", "FizzBuzz" or a number is output between 1 and 100.', () => {
   
-  test('FizzBuzz must be output when it is a multiple of 15.', () => {
-    [15, 45, 90].forEach(number => {
-      expect(conversionToFizzBuzz(number)).toBe('Fizz Buzz');
-    });
-    [3, 10, 85].forEach(number => {
-      expect(conversionToFizzBuzz(number)).not.toBe('Fizz Buzz');
-    });
-  });
-  test('Buzz must be output when it is a multiple of 5.', () => {
-    [5, 10, 100].forEach(number => {
-      expect(conversionToFizzBuzz(number)).toBe('Buzz');
-    });
-    [3, 15, 90].forEach(number => {
-      expect(conversionToFizzBuzz(number)).not.toBe('Buzz');
-    });
-});
-  test('Fizz must be output when it is a multiple of 3.', () => {
-    [3, 6, 99].forEach(number => {
-      expect(conversionToFizzBuzz(number)).toBe('Fizz');
-    });
-    [5, 15, 90].forEach(number => {
-      expect(conversionToFizzBuzz(number)).not.toBe('Fizz');
+  test.each`
+  number | expected
+  ${[15, 30 ,45]} | ${"Fizz Buzz"}
+  ${[5, 10, 100]} | ${"Buzz"}
+  ${[3, 6, 99]} | ${"Fizz"}
+`("$expected must be output when it is a multiple of $number.", ({ number:number, expected } : {number:number[], expected: number}) => {
+    number.forEach(number => {
+      expect(conversionToFizzBuzz(number)).toBe(expected);
     });
   });
 
